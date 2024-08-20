d79f08766929325baa5aca5c57759c0f3d6da3ae
Neetish456 <84724911+Neetish456@users.noreply.github.com>
2024-08-20 22:15:16 +0530

    Add files via upload
    assignment day 2

---
+++ python/p10.py
@@ -0,0 +1,7 @@
+#problem statement: Write a porgram to check if the given Alphabet is Vowel or Consonants.
+list1 = [a,e,i,o ,u]
+alphabet1 = str(input("Enter the alphabet : "))
+if alphabet1 in list1:
+    print("The given Alphabet is Vowel.")
+else:
+    print("The given Alphabet is Consonant.")
