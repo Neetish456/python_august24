4a16b9b511ef063ee2d72a0e24d80a58b97dbef7
Neetish456 <84724911+Neetish456@users.noreply.github.com>
2024-08-20 22:03:48 +0530

    Add files via upload
    assignment day 2

---
+++ python/p7.py
@@ -0,0 +1,14 @@
+#problem statement: Write a program to check if the number is a Perfect square.
+import math
+
+print("This program checks if the given number is a perfect square.")
+number1 = int(input("Enter the number : "))
+
+square_root = math.sqrt(number1)
+
+perfect_square = float(square_root**2)
+if number1 == perfect_square :
+    print("The given number is a perfect square. ")
+
+
+
