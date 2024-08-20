41318275a9775a7d51c5fdb1a0a3734513ddaf18
Neetish456 <84724911+Neetish456@users.noreply.github.com>
2024-08-20 21:51:13 +0530

    Add files via upload

---
+++ python/p6.py
@@ -0,0 +1,6 @@
+#problem statement : Write a program to check if the Alphabet is Uppercase.
+
+alphabet = str(input("Enter the Aplhabet to check if its Uppercase : "))
+
+if alphabet.isupper():
+    print("The given Alphabet is Uppercase.")
